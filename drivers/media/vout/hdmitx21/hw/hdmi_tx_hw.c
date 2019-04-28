9af4df699215bf5175f93398fa9b61faf40e479e
Arthur Liberman <arthur_liberman@hotmail.com>
2019-04-28 23:35:34 +0300

    hdmi_audio: fix passthrough for 4k DD+ This hack is no longer required, as hdmi_get_aud_n_paras() now handles the multiplier correctly.

--- drivers/media/vout/hdmitx21/hw/hdmi_tx_hw.c
+++ drivers/media/vout/hdmitx21/hw/hdmi_tx_hw.c
@@ -1915,14 +1915,6 @@ static void set_aud_acr_pkt(struct aud_para *audio_param)
 	if (hdev->frl_rate)
 		aud_n_para = hdmi21_get_frl_aud_n_paras(audio_param->rate, hdev->frl_rate);
 	hdmitx21_set_reg_bits(ACR_CTS_CLK_DIV_IVCTX, hdev->frl_rate ? 1 : 0, 4, 1);
-	/* N must multiples 4 for DD+ */
-	switch (audio_param->type) {
-	case CT_DOLBY_D:
-		aud_n_para *= 4;
-		break;
-	default:
-		break;
-	}
 	HDMITX_INFO("audio: aud_n_para = %d\n", aud_n_para);
 	hdmitx21_wr_reg(N_SVAL1_IVCTX, (aud_n_para >> 0) & 0xff); //N_SVAL1
 	hdmitx21_wr_reg(N_SVAL2_IVCTX, (aud_n_para >> 8) & 0xff); //N_SVAL2
@@ -4875,4 +4867,3 @@ static void hdmitx21_color_convert(struct hdmitx_dev *hdev, u32 output_color_for
 		HDMITX_DEBUG("csc not support/implemented yet\n");
 	}
 }
-
