4088584d320bfd04ee6bd9baa2b83bfb9df067e1
Portisch <hugo.portisch@yahoo.de>
2021-02-02 12:23:24 +0100

    aocec: do not parse received messages in CECB when controlled by system

--- drivers/media/cec/hdmi_aocec_api.c
+++ drivers/media/cec/hdmi_aocec_api.c
@@ -605,6 +605,10 @@ void cec_message_op(unsigned char *msg, unsigned char len)
 	unsigned int vendor_id;
 	unsigned int a, b = 0;
 	struct spd_device_info *info;
+
+	if (cec_dev->hal_flag & (1 << HDMI_OPTION_SYSTEM_CEC_CONTROL))
+		return true;
+
 	//cec handle just for tv product & need port mapping
 	if (cec_dev->dev_type != CEC_TV_ADDR || cec_dev->port_seq == 0)
 		return;
@@ -2788,4 +2792,3 @@ inline unsigned int get_pin_status(void)
 
 	return reg;
 }
-
