37984b38913f5bed436c04d83659561199d987d3
Portisch <hugo.portisch@yahoo.de>
2022-03-31 15:31:12 +0200

    hdmitx: line up handling of 'aud_output_ch' 'aud_output_ch' got set to 0 on stereo after multi channel playback

--- drivers/media/vout/hdmitx_common/hdmitx_audio.c
+++ drivers/media/vout/hdmitx_common/hdmitx_audio.c
@@ -767,8 +767,7 @@ static void hdmitx_set_i2s_mask(struct aud_para *tx_aud_param, char ch_num, char
 		tx_aud_param->aud_output_i2s_ch = 0;
 		return;
 	}
-	if (!(ch_num == 2 || ch_num == 4 ||
-	      ch_num == 6 || ch_num == 8)) {
+	if (!ch_num || !(ch_num % 2 == 0)) {
 		ch_num = 2;
 		HDMITX_ERROR("audio: chn setting, must be 2, 4, 6 or 8, Rst as def 2\n");
 	}
@@ -776,7 +775,7 @@ static void hdmitx_set_i2s_mask(struct aud_para *tx_aud_param, char ch_num, char
 		ch_msk = 1;
 		HDMITX_ERROR("audio: chn msk, must larger than 0, Rst as def 1\n");
 	}
-	update_flag = (ch_num << 4) + ch_msk;
+	update_flag = ((ch_num << 4) & 0xf0) | (ch_msk & 0xf);
 	tx_aud_param->aud_output_i2s_ch = update_flag;
 }
 
