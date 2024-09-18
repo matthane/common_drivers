2dc2316eddc1c03f031610b0131e8ae00d806289
Portisch <hugo.portisch@yahoo.de>
2024-09-18 15:25:50 +0200

    eth: call RTL8211F function on resume

--- drivers/net/ethernet/stmicro/stmmac/dwmac-meson8b.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-meson8b.c
@@ -775,6 +775,7 @@ static int meson8b_suspend(struct device *dev)
 	return ret;
 }
 
+extern int rtl821x_resume(struct phy_device *phydev);
 static int meson8b_resume(struct device *dev)
 {
 	struct net_device *ndev = dev_get_drvdata(dev);
@@ -814,6 +815,7 @@ static int meson8b_resume(struct device *dev)
 		if (phydev)
 			phydev->irq_suspended = 0;
 		ret = stmmac_resume(dev);
+		ret = rtl821x_resume(phydev);
 		/*this flow only for txhd2, not for common anymore*/
 		if (phy_mode == 2)
 			stmmac_global_err(priv);
