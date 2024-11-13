86e80be8409331ac17933de40f00c9991afeb99b
Portisch <hugo.portisch@yahoo.de>
2024-11-13 14:38:31 +0100

    hdmitx: ignore hdr_priority flag There is no need to cut off DV/HDR capability from display function.

--- drivers/media/vout/hdmitx21/hdmi_tx_main.c
+++ drivers/media/vout/hdmitx21/hdmi_tx_main.c
@@ -4381,6 +4381,7 @@ static int amhdmitx21_device_init(struct hdmitx_dev *hdev)
 
 	hdev->tx_comm.hdmi_last_hdr_mode = 0;
 	hdev->tx_comm.hdmi_current_hdr_mode = 0;
+	hdev->tx_comm.hdr_priority = 0;
 
 	/*
 	 * hdr/vsif packet status init, no need to get actual status,
